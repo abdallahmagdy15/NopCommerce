633be19865cb3c5d05447517c91996f326389953
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-06-24 17:22:27 +0200

    address geolocation plugin added

---
+++ Libraries/Nop.Core/Domain/Common/AddressLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Core.Domain.Common
+{
+    class AddressLocation: BaseEntity
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
---
+++ Plugins/Nop.Plugin.Payment.AddressLocation/AddressGeolocationPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Nop.Core;
+using Nop.Services.Common;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Payment.AddressLocation
+{
+    public class AddressGeolocationPlugin : BasePlugin
+    {
+        private readonly IWebHelper _webHelper;
+        public AddressGeolocationPlugin(IWebHelper webHelper)
+        {
+            _webHelper = webHelper;
+        }
+        public override string GetConfigurationPageUrl()
+        {
+            return $"{_webHelper.GetStoreLocation()}Admin/AddressGeolocation/Configure";
+        }
+        public override Task InstallAsync()
+        {
+            return base.InstallAsync();
+        }
+        public override Task UninstallAsync()
+        {
+            return base.UninstallAsync();
+        }
+    }
+}
