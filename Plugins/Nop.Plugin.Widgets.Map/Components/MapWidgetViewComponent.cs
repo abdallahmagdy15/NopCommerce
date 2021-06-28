4ec64cce727722de8dbdec8ea21fe6e0da968e47
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-06-28 11:42:45 +0200

    addressgeolocation plugin deleted

--- Plugins/Nop.Plugin.Widgets.Map/Components/MapWidgetViewComponent.cs
+++ Plugins/Nop.Plugin.Widgets.Map/Components/MapWidgetViewComponent.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Nop.Web.Framework.Components;
 
 namespace Nop.Plugin.Widgets.Map.Components
-{ 
+{
     class MapWidgetViewComponent : NopViewComponent
     {
         public IViewComponentResult Invoke()
